0d65e810e98e96eea0ee489d5b82f6cebc340857
Yashaswi Chahande <chahandeyashaswi@gmail.com>
2025-01-28 21:47:54 +0530

    conditional rendering

--- reactbasic/src/Component/Card.jsx
+++ reactbasic/src/Component/Card.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 function Card() {
     const data = [
-        {image: 'https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?q=80&w=1196&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' , name: "Amazon basics", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultricies, nunc eu tristique semper."},
-        {image: 'https://plus.unsplash.com/premium_photo-1668418188837-d40b734ed6d2?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' , name: "Daily Objects", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultricies."},
-        {image: 'https://images.unsplash.com/photo-1567306226416-28f0efdc88ce?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' , name: "Apple", description: "Lorem ipsum dolor sit amet."}
+        {image: 'https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?q=80&w=1196&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' , name: "Amazon basics", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultricies, nunc eu tristique semper." , instock: true},
+        {image: 'https://plus.unsplash.com/premium_photo-1668418188837-d40b734ed6d2?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' , name: "Daily Objects", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultricies." , instock: false},
+        {image: 'https://images.unsplash.com/photo-1567306226416-28f0efdc88ce?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' , name: "Apple", description: "Lorem ipsum dolor sit amet. " , instock: true }
         
     ]
   return (
@@ -20,6 +20,7 @@ function Card() {
             <p className='text-xs mt-5'>
             {elem.description}
             </p>
+            <button className= {`px-4 py-1 ${elem.instock ? 'bg-sky-600' : 'bg-red-600'} text-xs rounded mt-3 text-zinc-100`}>{elem.instock ? "In Stock" : "Out of Stock"}</button>
         
         </div>
       </div>
