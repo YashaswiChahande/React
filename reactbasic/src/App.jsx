c3bdf89f5089228c5144daef624a0e54fc4693d8
Yashaswi Chahande <chahandeyashaswi@gmail.com>
2025-01-29 15:49:20 +0530

    useState

--- reactbasic/src/App.jsx
+++ reactbasic/src/App.jsx
@@ -1,16 +1,15 @@
-import React from "react";
-import Card from "./Component/Card"
+import React, { useState } from "react";
 
 
 function App (){
+  const [val, setVal] = useState(12);
+
   return (
-    <div>
-      <Card />
+    <div className="p-4">
+      <h1>{val}</h1>
+      <button onClick={()=>setVal((prev)=>prev+1)} className="px-3 py-1 bg-blue-500 rounded-full text-zinc-50">Change Value</button>
     </div>
   )
-    
-  
 }
-
 
 export default App
