8f2d3f9aac5c6b5d621af093638f46d566910b2b
Yashaswi Chahande <chahandeyashaswi@gmail.com>
2025-01-29 16:37:24 +0530

    useState

--- reactbasic/src/App.jsx
+++ reactbasic/src/App.jsx
@@ -8,7 +8,9 @@ function App (){
     <div className="p-4">
       <h1>name : {val.name}</h1>
       <h2>banned : {val.isBanned.toString()}</h2>
-      <button onClick={()=>setVal({...val, isBanned: !val.isBanned})} className="px-3 py-1 bg-blue-500 rounded-full text-white">Change Value</button>
+      <button onClick={()=>setVal({...val, isBanned: !val.isBanned})} className={`px-3 py-1 ${
+        val.isBanned ? "bg-red-500" : "bg-blue-600"
+        } rounded-full text-white`}>Change </button>
     </div>
   )
 }
