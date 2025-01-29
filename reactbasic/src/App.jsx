9f90423890973fde84640fbec5985a446c366fe6
Yashaswi Chahande <chahandeyashaswi@gmail.com>
2025-01-29 16:03:45 +0530

    useState

--- reactbasic/src/App.jsx
+++ reactbasic/src/App.jsx
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 
 
 function App (){
-  const [val, setVal] = useState(12);
+  const [val, setVal] = useState({name: 'harsh', isBanned: false});
 
   return (
     <div className="p-4">
-      <h1>{val}</h1>
-      <button onClick={()=>setVal((prev)=>prev+1)} className="px-3 py-1 bg-blue-500 rounded-full text-zinc-50">Change Value</button>
+      <h1>name : {val.name}</h1>
+      <h2>banned : {val.isBanned.toString()}</h2>
+      <button onClick={()=>setVal({...val, isBanned: !val.isBanned})} className="px-3 py-1 bg-blue-500 rounded-full text-white">Change Value</button>
     </div>
   )
 }
