c1c3a884314388eefcca37c84cd09b4d861b17d5
Yashaswi Chahande <chahandeyashaswi@gmail.com>
2025-01-30 19:51:07 +0530

    useState hook Creation

--- reactbasic/src/App.jsx
+++ reactbasic/src/App.jsx
@@ -1,18 +1,10 @@
-import React, { useState } from "react";
-
+import React  from "react";
+import Card from "./Component/Card";
 
 function App (){
-  const [val, setVal] = useState([{name:"harsh", age:24}, {name: "shivam", age:25}, {name: "kohli", age:32}]);
-
   return (
-    <div className="p-5">
-      {val.map(item => (
-        <div>
-          <h1>{item.name}</h1>
-          <h1>{item.age}</h1>
-      </div>
-    ))}
-      <button onClick={()=>setVal(() => val.map(item => item.name === "shivam" ? ({name: "shivam", age:26}): item))} className= 'px-3 py-1  bg-blue-600 rounded-full text-white'>Change </button>
+    <div>
+      <Card />
     </div>
   )
 }
--- reactbasic/src/Component/Card.jsx
+++ reactbasic/src/Component/Card.jsx
@@ -1,31 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaArrowRightLong } from "react-icons/fa6";
 
 function Card() {
 
-    const data = [
-        {
-            name: 'Mahiya Ve', description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
-        },
-        {
-            name: 'Junoon', description: "Lorem ipsum dolor sit amet, elit. "
-        },
-    ]
-
-    const handleClickDownload = ()=>{alert("Download Started.....")};
+  const [val, setVal] = useState(false);
 
   return (
-    <div className='w-full h-screen bg-zinc-300  flex flex-col gap-10 items-center justify-center'>
-      {data.map((item, index)=> (
-        <div key = {index} className='w-90 px-3 py-2 bg-zinc-100 rounded-md'>
-        <h3 className='font-semibold text-xl'>{item.name}</h3>
-        <p className='text-xs mt-2'>{item.description}
-        </p>
-        <button onClick={handleClickDownload} className='px-2 py-1 bg-blue-400 text-xs font-semibold text-zinc-100 rounded mt-3'>Download Now</button>
+    <div className='w-full h-screen bg-zinc-300 flex justify-center items-center'>
+      <div className=' relative w-60 h-32 rounded-md flex overflow-hidden '>
+        <img className= {`shrink-0 transition-transform duration-700 ease-in-out ${val === false ? "-translate-x-[0%]" : "-translate-x-[100%]"} w-full h-full object-cover`} src='https://images.unsplash.com/photo-1735149050406-76f49ced66e1?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+
+        <img className={`shrink-0 transition-transform duration-700 ease-in-out ${val === false ? "-translate-x-[0%]" : "-translate-x-[100%]"} w-full h-full object-cover`} src='https://images.unsplash.com/photo-1735124283566-5f5707a40808?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+        
+        <span onClick={() => setVal( () => !val )} className='w-8 h-8 flex items-center justify-center rounded-full absolute bottom-[0%] bg-[#dadada7b] left-1/2 -translate-x-[50%] -translate-y-[50%]' >
+        <FaArrowRightLong size={".7em"} />
+        </span>
+        
       </div>
-      ))}
+      
     </div>
   )
 }
 
 export default Card
-
