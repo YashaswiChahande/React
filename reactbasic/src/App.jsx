80b298ac4d2aa3a36af5a53663fe1dba869f3623
Yashaswi Chahande <chahandeyashaswi@gmail.com>
2025-01-30 18:25:38 +0530

    useState array addition

--- reactbasic/src/App.jsx
+++ reactbasic/src/App.jsx
@@ -2,12 +2,17 @@ import React, { useState } from "react";
 
 
 function App (){
-  const [val, setVal] = useState([1,2,3,4,5,6]);
+  const [val, setVal] = useState([{name:"harsh", age:24}, {name: "shivam", age:25}, {name: "kohli", age:32}]);
 
   return (
     <div className="p-5">
-      {val.map(item => <h1>{item}</h1>)}
-      <button onClick={()=>setVal(() => val.filter((item,index) => index != 2))} className= 'px-3 py-1  bg-blue-600 rounded-full text-white'>Change </button>
+      {val.map(item => (
+        <div>
+          <h1>{item.name}</h1>
+          <h1>{item.age}</h1>
+      </div>
+    ))}
+      <button onClick={()=>setVal(() => val.map(item => item.name === "shivam" ? ({name: "shivam", age:26}): item))} className= 'px-3 py-1  bg-blue-600 rounded-full text-white'>Change </button>
     </div>
   )
 }
