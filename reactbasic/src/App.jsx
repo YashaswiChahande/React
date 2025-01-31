b2b694e65a2ee701709f3650d915adcffd56ecf2
Yashaswi Chahande <chahandeyashaswi@gmail.com>
2025-01-31 08:57:15 +0530

    props addfriends

--- reactbasic/src/App.jsx
+++ reactbasic/src/App.jsx
@@ -1,11 +1,37 @@
-import React  from "react";
+import React, { useState }  from "react";
 import Card from "./Component/Card";
 
-function App (){
+function App (){ 
+
+  const data = [
+    {name: "John", profession: "Painter", image: "https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", friends: false},
+    {name: "Romeo", profession: "Artist", image: "https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fHBvcnRyYWl0fGVufDB8fDB8fHww",friends: false },
+    {name: "jobless", profession: "Entreprenuar", image: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", friends: false},
+    {name: "Koby", profession: "Racist", image: "https://images.unsplash.com/photo-1504257432389-52343af06ae3?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", friends: false}
+  ]
+
+      const [realdata, setRealData] = useState(data);
+      const handleFriendsButton = (cardindex)=> {
+        setRealData((previous) => {
+          return previous.map((item, index) => {
+            if( index === cardindex ){
+              return {...item, friends: !item.friends}
+            }
+            return item;
+          })
+        })
+      }
   return (
     <>
-      <Card text="Know More" color="bg-blue-600" />
-      <Card text="Download Now" color="bg-red-700" />
+    <div className="w-full h-screen bg-zinc-300 flex gap-4 items-center justify-center">
+
+    {realdata.map((item, index) => (
+      // <Card image = {item.image} name = {item.name} profession = {item.profession} normal way />
+      <Card key = {index} index={index} handleClick = {handleFriendsButton} values = {item} />
+    ))}
+
+    </div>
+     
     </>
   )
 }
--- reactbasic/src/Component/Card.jsx
+++ reactbasic/src/Component/Card.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
-function Card({text, color}) {
+function Card({values, handleClick, index}) {
+
+  const {name, image, profession, friends} = values;
+
+
   return (
-    <button className={`px-3 py-1 ${color} text-zinc-100 text-xs rounded m-2`} >{text}</button>
+    <div className='w-52 bg-white rounded-md overflow-hidden'>
+      <div className='w-full h-60 bg-sky-200'>
+        <img className='w-full h-full object-cover object-[center_top]' src={image} alt=""/>
+      </div>
+      <div className='w-full p-3'>
+      <h3 className='text-xl font-semibold'>{name}</h3>
+      <h5 className='text-xs'>{profession}</h5>
+      <button onClick = {()=> handleClick (index)} className=' mt-4 px-3 py-1 text-xs text-white bg-blue-500 font-semibold rounded-md' >{ friends === true ? "Friends" : "Add Friend"} </button>
+      </div>
+      
+    </div>
   )
 }
 export default Card
