6a5b7231bcbd13a1dd67e3b177beb5708e2b9e41
Yashaswi Chahande <chahandeyashaswi@gmail.com>
2025-01-31 09:57:39 +0530

    props addfriends

--- reactbasic/src/Component/Card.jsx
+++ reactbasic/src/Component/Card.jsx
@@ -13,10 +13,11 @@ function Card({values, handleClick, index}) {
       <div className='w-full p-3'>
       <h3 className='text-xl font-semibold'>{name}</h3>
       <h5 className='text-xs'>{profession}</h5>
-      <button onClick = {()=> handleClick (index)} className=' mt-4 px-3 py-1 text-xs text-white bg-blue-500 font-semibold rounded-md' >{ friends === true ? "Friends" : "Add Friend"} </button>
+      <button onClick = { ()=> handleClick (index)} className= {`mt-4 px-3 py-1 text-xs text-white ${friends ? "bg-green-600" : "bg-blue-500"} font-semibold rounded-md`} >{ friends === true ? "Friends" : "Add Friend"} </button>
       </div>
       
     </div>
   )
 }
+
 export default Card
