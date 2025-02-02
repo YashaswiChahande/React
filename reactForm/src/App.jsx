9a0c6e72671471f85cbe57b8056513d0721511d0
Yashaswi Chahande <chahandeyashaswi@gmail.com>
2025-02-02 17:36:46 +0530

    Form ControlledComponent

--- reactForm/src/App.jsx
+++ reactForm/src/App.jsx
@@ -1,23 +1,23 @@
-import React, {useRef} from 'react'
+import React, {useState} from 'react'
 
 function App() {
 
-  const name = useRef(null);
-  const age = useRef(null);
-  const email = useRef(null);
+  const [val, setVal] = useState({name: ""})
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(name.current.value, age.current.value, email.current.value);//name.current for name .value to get the value
+    console.log(val)
   }
 
   return (
-    <form action='' onSubmit={handleSubmit}>
-      <input ref={name} placeholder='name' type='text' />
-      <input ref={age} placeholder='age' type='text' />
-      <input ref={email} placeholder='Email' type='text' />
-      <input type='submit'/>
-    </form>
+    <div>
+      <form action = "" onSubmit={handleSubmit}>
+        <input onChange = { (event) => setVal ({...val, name: event.target.value}) } type = "text" placeholder='name' />
+
+        <input onChange = { (event) => setVal ({...val, email: event.target.value}) } type = "email" placeholder='email' />
+        <input type= "submit" />
+      </form>
+    </div>
   )
 }
 
